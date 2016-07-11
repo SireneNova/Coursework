b0b9ffce171510323acf57488c1c1c6619e646c3
rebeccapizano <rebeccapizano@users.noreply.github.com>
2016-07-10 22:41:52 -0700

    Add files via upload

---
+++ CSharp/LINQtutorial/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EastwoodLINQtutorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sample = "I enjoy writing uber-software in C#";
+
+            //vowels out of sen
+            var result = from c in sample.ToLower()
+                         where c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u'
+                         orderby c
+                         group c by c;
+                         //orderby c //alphabetical order/ascending, can do descending too
+                         //select c; //don't need select when using group
+
+            foreach (var item in result) //with group, result is a grouped enumerable
+            {
+                //Console.WriteLine(item);
+                //Console.WriteLine(item.Key); //need this with group
+                Console.WriteLine("{0} - {1}", item.Key, item.Count()); //first param, second
+            }
+
+        }
+    }
+}
---
+++ CSharp/LINQtutorial/Program2.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EastwoodLINQtutorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var people = new List<Person>
+            {
+                new Person{FirstName="John", LastName = "Doe", Age = 25},
+                new Person{FirstName="Jane", LastName = "Doe", Age = 26},
+                new Person{FirstName="John", LastName = "Williams", Age = 40},
+                new Person{FirstName="Samantha", LastName = "Williams", Age = 34},
+                new Person{FirstName="Bob", LastName = "Walters", Age = 12},
+            };
+
+            var result = from p in people
+                         //where p.Age < 30 && p.LastName=="Doe"
+                         orderby p.LastName descending
+                         group p by p.LastName;
+                         //select p;
+
+            foreach (var item in result) //with group, result is a grouped enumerable
+            {
+                //Console.WriteLine("{0} - {1}", item.FirstName, item.LastName); //gives number
+                Console.WriteLine(item.Key + " - " + item.Count());
+                foreach (var p in item)
+                {
+                    Console.WriteLine("\t{0}, {1}", p.LastName, p.FirstName);
+                }
+            }
+
+        }
+    }
+
+    public class Person
+    {
+        public string FirstName { get; set; } //prop, then press tab twice
+        public string LastName { get; set; }
+        public int Age { get; set; }
+    }
+}
