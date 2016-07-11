8b2d200e656c31efe5c57cae6d72ae953fd11c22
rebeccapizano <rebeccapizano@users.noreply.github.com>
2016-07-10 22:05:46 -0700

    Add files via upload

---
+++ CSharp/SerializationDeserialization/HomeObject.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SerializingDemo
+{
+    [Serializable]
+    class HomeObject
+    {
+        int yearBuilt = 0;
+        public enum TYPE { SINGLE_FAMILY, TOWN_HOME, CONDO };
+        TYPE htype;
+
+        public HomeObject()
+        {
+        }
+
+        public void setYear(int year)
+        {
+            yearBuilt = year;
+        }
+
+        public int getYear()
+        {
+            return yearBuilt;
+        }
+
+        public TYPE getHomeType()
+        {
+            return htype;
+        }
+
+        public void setHomeType(TYPE type_)
+        {
+            htype = type_;
+        }
+
+        public string printContent()
+        {
+            string msg = "HomeType: " + htype + "Year Built: " + yearBuilt;
+            return msg;
+        }
+    }
+}
---
+++ CSharp/SerializationDeserialization/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SerializingDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SerializeData sdata = new SerializeData("testBinaryFile.txt");
+
+            HomeObject home = new HomeObject();
+            home.setHomeType(HomeObject.TYPE.CONDO);
+            home.setYear(2000);
+
+            ComputerObject co = new ComputerObject();
+            co.setType(ComputerObject.PC_TYPE.LINUX);
+            co.setCost(0.0);
+
+            ComputerObject co1 = new ComputerObject();
+            co1.setType(ComputerObject.PC_TYPE.WINDOWS);
+            co1.setCost(100.0);
+
+            sdata.SerializeObject(home);
+            sdata.SerializeObject(co);
+            sdata.SerializeObject(co1);
+
+            sdata.closeStream();
+
+            sdata.DeserializeObjects();
+            sdata.closeStream();
+         }
+    }
+}
