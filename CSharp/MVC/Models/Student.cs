607a6bce4a39c5498f8f53affe16af2f464952ba
Rebecca Pizano <rebeccapizano@users.noreply.github.com>
2016-12-03 20:50:55 -0800

    Add files via upload

---
+++ CSharp/MVC/Models/Student.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ContosoUniversity.Models
+{
+    public class Student
+    {
+        public int ID { get; set; }
+        //can be simply ID or classnameID
+        public string LastName { get; set; }
+        public string FirstMidName { get; set; }
+        public DateTime EnrollmentDate { get; set; }
+
+        public virtual ICollection<Enrollment> Enrollments { get; set; }
+        //navigation property, will hold entities related to enrollment. studentID and primary key.
+        //virtual to allow for lazy loading and similar EF functionality
+    }
+}
