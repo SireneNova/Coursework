3ec81dd1032f5ee1c51d98bf4fc641f48f33c390
Sirene R Nova <12014557+SireneNova@users.noreply.github.com>
2022-08-01 12:28:31 -0700

    Create countinSort.py

---
+++ Python/Codility/countinSort.py
@@ -0,0 +1,13 @@
+# A way of sorting a list containing values in the set {0, 1...k}. This interested me because it is relativeley efficient O(n+k while have a nested for loop:
+
+def countingSort(A, k):
+    n = len(A)
+    count = [0] * (k + 1)
+    for i in range(n):
+        count[A[i]] += 1
+        p = 0
+    for i in range(k + 1):
+        for j in range(count[i]):
+            A[p] = i
+            p += 1
+    return A
