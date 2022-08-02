e6b5e88e3b51a025290b4ef8b28045f8509a29f5
Sirene R Nova <12014557+SireneNova@users.noreply.github.com>
2022-08-01 17:26:07 -0700

    Update countingSort.py

--- Python/Codility/countingSort.py
+++ Python/Codility/countingSort.py
@@ -1,4 +1,5 @@
-# A way of sorting a list containing values in the set {0, 1...k}. This interested me because it is relativeley efficient O(n+k) while have a nested for loop:
+# A way of sorting a list containing values in the set {0, 1...k}. This interested me because it is relativeley efficient O(n+k) while have a nested for loop.
+# Kind of an odd way to write it:
 
 def countingSort(A, k):
     n = len(A)
@@ -7,7 +8,16 @@ def countingSort(A, k):
         count[A[i]] += 1
         p = 0
     for i in range(k + 1):
+        print("i: "+ str(i))
         for j in range(count[i]):
+            print("j: " + str(j))
+            print("p: " + str(p))
             A[p] = i
+            print(A[p])
             p += 1
     return A
+
+
+A = [0,2,5,6,4,7,3,1]
+
+print(countingSort(A, 7))
